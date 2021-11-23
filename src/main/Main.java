a9c2eff8fab4c47b263ec41531525af5f26bb5af
Dmitry <paladinko2@gmail.com>
2021-11-23 20:36:30 +0300

    comm without APP_CLASS

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("First main commit");
+
+        System.out.println("commit without APP_CLASS");
     }
 }
